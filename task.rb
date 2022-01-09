e42cc9f2d5e845a666266087306eeb8f09f9b91b
emily doswell <emilydoswell@yahoo.co.uk>
2022-01-09 10:26:58 +0100

    added URL

--- task.rb
+++ task.rb
@@ -5,6 +5,7 @@
 # Create an array of vendor item numbers / figure out how to store the vendor item numbers.
 # Create a loop
 # Insert the vendor item number into the URL
+"https://www.mammut.com/uk/en/products/#{vendor-item-number}"
 # Locate the description inside the html of the product page
 # Scrape the webpage for that location
 # Pull the decription from the HTML & Print it corresponding to its vendor item number
