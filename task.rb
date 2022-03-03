edff5882a88e95186a1842d56a45a5f2801197fb
emily doswell <emilydoswell@yahoo.co.uk>
2022-03-03 14:49:19 +0100

    to do

--- task.rb
+++ task.rb
@@ -26,7 +26,7 @@ vin.each do |vendoritemnumber|
   end
 end
 
-# To do
+# To do  - To do list here
 
 # Insert the vendor item number into the URL
 # "https://www.mammut.com/uk/en/products/#{vendor-item-number}"
