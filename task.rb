e494ad094fef5e150131bf50f72d2163d03f0822
emily doswell <emilydoswell@yahoo.co.uk>
2022-03-03 14:46:04 +0100

    to do

--- task.rb
+++ task.rb
@@ -1,22 +1,36 @@
-# require 'open-uri'
-# require 'nokogiri'
+require 'open-uri'
+require 'nokogiri'
 require 'csv'
 
-filepath = "vin.csv"
-
-CSV.foreach(filepath) do |row|
-  puts "#{row[0]}"
-end
-
-
-# The Plan
 # Create an array of vendor item numbers / figure out how to store the vendor item numbers.
 # Loop through csv file to pull out each VIN one by one
 # vendor-item-number-array = []
 
+filepath = "vin.csv"
+vin = []
+
+CSV.foreach(filepath) do |row|
+  puts "#{row[0]}"
+  vin << row[0]
+end
+
+vin.each do |vendoritemnumber|
+  url = "https://www.mammut.com/uk/en/products/#{vendoritemnumber}"
+
+  html_file = URI.open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+  
+  html_doc.search('.PDPInfo_text__ndHaU').each do |element|
+    puts element.text.strip
+    puts element.attribute('href').value
+  end
+end
+
+# To do
 
 # Insert the vendor item number into the URL
 # "https://www.mammut.com/uk/en/products/#{vendor-item-number}"
+
 # Locate the description inside the html of the product page
 # class="PDPInfo_text__ndHaU"
 # Scrape the webpage for that location
@@ -25,12 +39,3 @@ end
 
 
 
-# url = "https://www.mammut.com/uk/en/products/#{vendor-item-number}"
-
-# html_file = URI.open(url).read
-# html_doc = Nokogiri::HTML(html_file)
-
-# html_doc.search('.PDPInfo_text__ndHaU').each do |element|
-#   puts element.text.strip
-#   puts element.attribute('href').value
-# end
